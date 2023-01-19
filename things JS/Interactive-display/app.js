e102c5e8adb8c30aeeb81c248ed37447319ebbd5
John Doe <edixon20pes@gmail.com>
2023-01-18 22:40:40 -0400

    [ADD] card Element

--- things JS/Interactive-display/app.js
+++ things JS/Interactive-display/app.js
@@ -11,14 +11,14 @@ const reviews = [
     },
 
     {
-        img: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        img: 'https://media.istockphoto.com/id/1322123064/es/foto/retrato-de-un-adorable-gato-blanco-con-gafas-de-sol-y-camisa-yace-sobre-una-hamaca-de-tela.webp?s=612x612&w=is&k=20&c=WtiESf62DW-P0YjdThVK3S2cOwNbpvkifIk0NA0SdTQ=',
         alt: 'Profile picture of serius cat',
         text: 'This is a test, JS is the best for WD! ',
         name: 'Moon cat'
     },
 
     {
-        img: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        img: 'https://media.istockphoto.com/id/682668918/es/foto/gato-usando-gafas-de-sol-relajantes-sentarse-en-la-tumbona.jpg?s=612x612&w=is&k=20&c=O4on886g_sUMZosxuajrRH8lf5aGwaF4vu0wlYUKDcU=',
         alt: 'Profile picture of serius cat',
         text: 'This is a test, JS is the best for WD! ',
         name: 'Moon cat'
@@ -29,6 +29,15 @@ function populateDisplay() {
     reviews.forEach(review => {
         const cardElement = document.createElement('div')
         cardElement.classList.add('card')
+
+        const imageContainer = document.createElement('div')        
+        imageContainer.classList.add('image-container')
+        const imageElement = document.createElement('img')
+        imageElement.setAttribute('src',review.img)
+        imageElement.setAttribute('alt',review.alt)
+        imageContainer.append(imageElement)
+        
+        cardElement.append((imageContainer))
         reviewContainer.append(cardElement)
     })
 }
